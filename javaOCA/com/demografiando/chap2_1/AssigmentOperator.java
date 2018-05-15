2d9b67c0ea3193e2c1589f40a50df929ab531dd4
ksaweryn <nirvomexvii@gmail.com>
2018-05-14 22:47:22 -0500

    Arithmetic operators

--- javaOCA/com/demografiando/chap2_1/AssigmentOperator.java
+++ javaOCA/com/demografiando/chap2_1/AssigmentOperator.java
@@ -7,3 +7,12 @@ class AssigmentOperator {
   //int i1 = l1;
 
 }
+
+class Foo {
+  public static void main (String args[]) {
+    boolean b1, b2, b3, b4, b5, b6;    // line 1
+    b1 = b2 = b3 = true;               // line 2
+    b4 = 0;                            // line 3
+    b5 = 'false';                      // line 4
+    b6 = yes;                          // line 5
+}
