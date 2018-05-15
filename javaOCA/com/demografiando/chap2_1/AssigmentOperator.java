53354a81371d305fac3d77202a159376cd1da3ee
ksaweryn <nirvomexvii@gmail.com>
2018-05-14 22:46:25 -0500

    Arithmetic operators

---
+++ javaOCA/com/demografiando/chap2_1/AssigmentOperator.java
@@ -0,0 +1,9 @@
+package com.demografiando.chap2_1;
+
+class AssigmentOperator {
+
+  long l1 = 234242424242L;
+  //It fails to compile, incompatible types
+  //int i1 = l1;
+
+}
---
+++ javaOCA/com/demografiando/chap2_1/WideningDataType.java
@@ -0,0 +1,17 @@
+package com.demografiando.chap2_1;
+
+public class WideningDataType {
+  byte b1 = 10;
+  byte b2 = 10;
+  static final byte b5 = 20;
+  static final byte b6 = 10;
+  // It fails to compile. Incompatible types.
+  //short s1 = b1 + b2;
+  public static void main (String... args){
+    byte b3 = 10;
+    byte b4 = 10;
+    short s1 = b5+b6;
+    System.out.println(b3+b4); //By widening it is int
+    System.out.println(s1); //By final nonaccess modifier it is short
+  }
+}
