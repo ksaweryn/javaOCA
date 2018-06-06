e4dd2c75b4b4a500b8fd088e217f99109db3d34b
ksaweryn <nirvomexvii@gmail.com>
2018-06-05 23:29:53 -0500

    Calling methods on objects

---
+++ javaOCA/com/demografiando/chap3/Star.java
@@ -0,0 +1,16 @@
+package com.demografiando.chap3;
+
+class Star {
+  double starAge;
+
+  // mutator
+  public void setStarAge(double newAge) {
+    starAge = newAge;
+  }
+
+  // accessor
+  public double getStarAge() {
+    return starAge;
+  }
+
+}
